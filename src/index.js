61e46409641b32372dd5fe57585c3e9de06d01dd
Sergeq-mouse-code <moisecode99@gmail.com>
2020-09-29 19:49:45 +0300

    init: start task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (Array.isArray(array && array.length > 1)) {}
+    return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    return 0;
 }
