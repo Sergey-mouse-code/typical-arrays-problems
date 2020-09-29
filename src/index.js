d80c8222ca9176bf1e7b3609923abb11d97ca41e
Sergeq-mouse-code <moisecode99@gmail.com>
2020-09-29 20:07:41 +0300

    feat: function min

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 exports.min = function min(array) {
-    if (Array.isArray(array && array.length > 1)) {}
+    // [0,1,2,5,3,6,1]
+    if (Array.isArray(array) && array.length >= 1) {
+        let length = array.length;
+        if (length === 1) return array;
+        for (let i = 1; i < length; i++) {
+            let searchIndex = i - 1;
+            let currentIndex = array[i];
+            while (searchIndex >= 0 && array[searchIndex] > currentIndex) {
+                array[searchIndex + 1] = array[searchIndex];
+                searchIndex--;
+
+            }
+            array[searchIndex + 1] = currentIndex;
+        }
+        return array[0];
+    }
     return 0;
 }
 
