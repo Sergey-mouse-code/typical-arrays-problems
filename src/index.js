9908b5de35265c5dac2ba0f43f73e111701d577c
Sergeq-mouse-code <moisecode99@gmail.com>
2020-09-29 20:08:41 +0300

    feat: add function max

--- src/index.js
+++ src/index.js
@@ -19,6 +19,22 @@ exports.min = function min(array) {
 }
 
 exports.max = function max(array) {
+    if (Array.isArray(array) && array.length >= 1) {
+        let length = array.length;
+        if (length === 1) return array;
+        for (let i = 1; i < length; i++) {
+            let searchIndex = i - 1;
+            let currentIndex = array[i];
+            while (searchIndex >= 0 && array[searchIndex] > currentIndex) {
+                array[searchIndex + 1] = array[searchIndex];
+                searchIndex--;
+
+            }
+            array[searchIndex + 1] = currentIndex;
+        }
+        return array[length - 1];
+    }
+
     return 0;
 }
 
